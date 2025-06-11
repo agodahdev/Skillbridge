5f80226c4618ed87f4da7fa883e6175b66a7aed0
agodahdev Goddah <godahahmed@gmail.com>
2025-06-11 08:08:12 +0000

    added lgin redirect setting

--- skillbridge/skillbridge_project/settings.py
+++ skillbridge/skillbridge_project/settings.py
@@ -126,3 +126,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Redirect unauthenicated users to this login page
+LOGIN_URL = '/accounts/login/'
+
+#After successful login, go to this URL
+LOGIN_REDIRECT_URL = '/'
+
