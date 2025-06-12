79d410b5ffd95c017a732996bf4936a6fc9f7724
agodahdev Goddah <godahahmed@gmail.com>
2025-06-12 15:26:42 +0000

    auto-login user after sign up

--- skillbridge/services/views.py
+++ skillbridge/services/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .forms import SkillServiceForm, BookingRequestForm
 from django.contrib.auth.decorators import login_required
 from .models import SkillService, BookingRequest
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # This view allows a logged-in user (provider) to submit a service 
 # @login required
@@ -25,7 +27,7 @@ def service_list(request):
     services = SkillService.objects.filter(is_approved=True)
 
     if query:
-        services = services.filter(title_icontains=query)
+        services = services.filter(title__icontains=query)
 
     if category and category != 'All':
         services = services.filter(category=category)
@@ -57,4 +59,15 @@ def book_service(request, service_id):
 
 def submission_success(request):
     return render(request, 'services/submission_success.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+           user = form.save()
+           login(request, user) #log in after signup
+           return redirect('/')
     
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
--- skillbridge/skillbridge_project/urls.py
+++ skillbridge/skillbridge_project/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('services/', include('services.urls')),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls'))
+   
+
 ]
