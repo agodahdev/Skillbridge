20d4db7e9cd7c8f99b3d79a99d8dd0d0b06def3d
agodahdev Goddah <godahahmed@gmail.com>
2025-06-15 14:59:42 +0000

    enable admin approval for services

--- skillbridge/services/admin.py
+++ skillbridge/services/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import SkillService, BookingRequest
 
-# Register your models here.
+@admin.register(SkillService)
+class SkillServiceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'provider', 'category', 'rate_per_hour', 'is_approved')
+    list_filter = ('is_approved', 'category',)
+    search_fields = ('title', 'description', 'provider__username')
+    actions = ['approve_services']
+
+    def approve_services(self, request, queryset):
+        queryset.update(is_approved=True)
+    approve_services.short_description = "Approve selected services"
+
+admin.site.register(BookingRequest)
