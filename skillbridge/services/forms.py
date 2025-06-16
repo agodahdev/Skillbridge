ed762ae7c9b750cc2c8c509131c9ab187609b2c3
agodahdev Goddah <godahahmed@gmail.com>
2025-06-16 05:27:58 +0000

    improved booking foprm with date picker and better layout

--- skillbridge/services/forms.py
+++ skillbridge/services/forms.py
@@ -13,3 +13,16 @@ class BookingRequestForm(forms.ModelForm):
     class Meta:
         model = BookingRequest
         fields = ['messages', 'requested_date']
+        widgets = {
+            'requested_date': forms.DateInput(
+                attrs={
+                    'type': 'date',
+                    'class': 'form-control'
+                }
+            ),
+            'messages': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Write a short message for the provider'
+            })
+
+        }
