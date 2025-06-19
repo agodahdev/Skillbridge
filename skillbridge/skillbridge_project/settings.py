945252e7141ce3e67adf1eb27ef015bb69963412
agodahdev Goddah <godahahmed@gmail.com>
2025-06-19 06:38:46 +0000

    update the sign up page with css styling

--- skillbridge/skillbridge_project/settings.py
+++ skillbridge/skillbridge_project/settings.py
@@ -29,7 +29,7 @@ import os
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    "8000-agodahdev-skillbridge-lok35d4n8qw.ws-eu120.gitpod.io",
+    "8000-agodahdev-skillbridge-15ctzq3u2r4.ws-eu120.gitpod.io",
 ]
 
 
@@ -136,5 +136,5 @@ LOGIN_REDIRECT_URL = '/services/dashboard'
 LOGOUT_REDIRECT_URL = '/'
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://8000-agodahdev-skillbridge-lok35d4n8qw.ws-eu120.gitpod.io",
+    "https://8000-agodahdev-skillbridge-15ctzq3u2r4.ws-eu120.gitpod.io",
 ]
