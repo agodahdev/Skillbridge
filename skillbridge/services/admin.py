c14ca5ba020ca8293d0cd6ae700f28a5bbbf2bdc
agodahdev Goddah <godahahmed@gmail.com>
2025-06-21 21:02:29 +0000

    added automated testing to readme

--- skillbridge/services/admin.py
+++ skillbridge/services/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import SkillService, BookingRequest
 
+
 @admin.register(SkillService)
 class SkillServiceAdmin(admin.ModelAdmin):
-    list_display = ('title', 'provider', 'category', 'rate_per_hour', 'is_approved')
+    list_display = ('title', 'provider', 'category', 'rate_per_hour',
+                    'is_approved')
     list_filter = ('is_approved', 'category',)
     search_fields = ('title', 'description', 'provider__username')
     actions = ['approve_services']
@@ -13,9 +15,9 @@ class SkillServiceAdmin(admin.ModelAdmin):
     approve_services.short_description = "Approve selected services"
 
 
-
 @admin.register(BookingRequest)
 class BookingRequestAdmin(admin.ModelAdmin):
-    list_display = ('service', 'client', 'requested_date', 'status', 'created_at')
+    list_display = ('service', 'client', 'requested_date',
+                    'status', 'created_at')
     list_filter = ('status', 'requested_date')
     search_fields = ('client_username', 'service__title')
