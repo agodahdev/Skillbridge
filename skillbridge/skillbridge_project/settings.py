74f478816753496e11a92ac9b5783f5e6a35a8f7
agodahdev Goddah <godahahmed@gmail.com>
2025-06-21 17:40:12 +0000

    fix issue with subnmit service issue

--- skillbridge/skillbridge_project/settings.py
+++ skillbridge/skillbridge_project/settings.py
@@ -29,7 +29,7 @@ import os
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    "8000-agodahdev-skillbridge-jtavredsm3y.ws-eu120.gitpod.io",
+    "8000-agodahdev-skillbridge-lr1r6ds6grc.ws-eu120.gitpod.io",
 ]
 
 
@@ -136,5 +136,5 @@ LOGIN_REDIRECT_URL = '/services/dashboard'
 LOGOUT_REDIRECT_URL = '/'
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://8000-agodahdev-skillbridge-jtavredsm3y.ws-eu120.gitpod.io",
+    "https://8000-agodahdev-skillbridge-lr1r6ds6grc.ws-eu120.gitpod.io",
 ]
