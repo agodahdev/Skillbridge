89a7865a4bf47b618275629b84c34ea485db0aa0
Ahmed Godah <ahmedgodah@hotmail.co.uk>
2025-10-14 16:57:31 +0100

    Fix services import path in main urls

--- skillbridge/skillbridge_project/urls.py
+++ skillbridge/skillbridge_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from services.views import home
+from skillbridge.services.views import home
 
 urlpatterns = [
     path('', home, name='home'),
