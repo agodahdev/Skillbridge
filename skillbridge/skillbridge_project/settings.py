653b419aac48da0d8105714b8077fe35b3f5c7cf
Ahmed Godah <ahmedgodah@hotmail.co.uk>
2025-10-14 14:49:33 +0100

    Move allowed hosts to environment file

--- skillbridge/skillbridge_project/settings.py
+++ skillbridge/skillbridge_project/settings.py
@@ -26,13 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-import os
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "skillbridge-ba237edbd42e.herokuapp.com",
-    ".herokuapp.com"
-]
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
